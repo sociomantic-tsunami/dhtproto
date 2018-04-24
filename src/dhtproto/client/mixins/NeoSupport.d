9c5fb33b8abed066529e0509b81cde086174bd52
Gavin Norman <gavin.norman@sociomantic.com>
2018-04-24 15:28:39 +0200

    Make DhtClient.DhtConnNotification public
    User code needs access to this symbol, as it's passed as an argument of the
    user connection notifier delegate.

--- src/dhtproto/client/mixins/NeoSupport.d
+++ src/dhtproto/client/mixins/NeoSupport.d
@@ -66,7 +66,7 @@ template NeoSupport ( )
 
         ***********************************************************************/
 
-        private alias SmartUnion!(DhtConnNotificationUnion) DhtConnNotification;
+        public alias SmartUnion!(DhtConnNotificationUnion) DhtConnNotification;
 
         /***********************************************************************
 
