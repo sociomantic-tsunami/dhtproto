7c6acfd72e1e232f530686a5622cee9be083ba18
Don.Clugston <don.clugston@dunnhumby.com>
2020-03-26 10:21:38 +0100

    Fix foreach variable shadowing warnings
    Rename foreach variables to avoid shadowing, which generates
    warnings in recent dmd.

--- src/dhtproto/client/UsageExamples.d
+++ src/dhtproto/client/UsageExamples.d
@@ -1223,11 +1223,11 @@ unittest
 
             // Per-request stats.
             auto rq_stats = this.dht.neo.new RequestStats;
-            foreach ( name, stats; rq_stats.allRequests() )
+            foreach ( name, s; rq_stats.allRequests() )
             {
                 // See swarm.neo.client.requests.Stats
                 this.log.info("{} {} requests handled, mean time: {}μs",
-                    stats.count, name, stats.mean_time_micros());
+                    s.count, name, s.mean_time_micros());
             }
         }
 
